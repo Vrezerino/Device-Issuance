9957fae81fee9dc94b3540d1604db8ef2886f29d
Patrick <parkpatrick1@gmail.com>
2024-02-20 19:35:40 +0200

    lisätty loggeri

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const app = express();
+const app = require('./app').app;
+const mongoose = require('./app').mongoose;
+const db = mongoose.connection;
+
+const logger = require('./utils/logger');
 const config = require('./utils/config');
 
-app.listen(config.PORT, () => {
-    console.log('Device Issuance server listening on port 8080.');
-});
+db.once('open', () => {
+    app.listen(config.PORT, () => {
+        logger.info('Device Issuance server listening on port 8080.');
+    });
+
+    io.on('connection', (socket) => {
+        logger.info('Client count:', io.engine.clientsCount)
+    })
+
+    io.engine.on('connection_error', (err) => {
+        logger.error(err.req)
+        logger.error(err.code)
+        logger.error(err.message)
+        logger.error(err.context)
+    })
+})
 
 module.exports = {
     app
--- server/utils/config.js
+++ server/utils/config.js
@@ -1,5 +1,18 @@
-const PORT = 8080;
+require('dotenv').config();
+
+const DB_URI = process.env.NODE_ENV === 'production' 
+	? process.env.DB_URI 
+	: (process.env.NODE_ENV === 'development' 
+		? process.env.DEVDB_URI 
+		: process.env.TESTDB_URI)
+
+const PORT = process.env.NODE_ENV === 'production' 
+	? process.env.PORT 
+	: (process.env.NODE_ENV === 'development' 
+		? process.env.DEVPORT 
+		: process.env.TESTPORT)
 
 module.exports = {
+    DB_URI,
     PORT
 }
